27841dda5603dffa1a9e0bf23107adc0ca0b7b2f
mario48 <info@mariuszb.eu>
2016-09-08 18:23:11 +0200

    pierwszy commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       $n = 5;
+       
+       $silnia = 1;
+       for ($i=1; $i<=$n; $i++) {
+         $silnia *= $i;
+       }
+       
+       if($n < 0)
+       {
+           echo "-1";
+       }
+ else {
+           echo ("silnia z $n = $silnia");
+}
+        ?>
+    </body>
+</html>
